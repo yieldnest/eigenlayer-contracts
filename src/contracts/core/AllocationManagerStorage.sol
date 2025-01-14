89522d9e678faba80babcc767fa1b25189b95327
Bowen Li <bowenli86@gmail.com>
2025-01-13 16:56:16 -0800

    fix: adjust storage gap

--- src/contracts/core/AllocationManagerStorage.sol
+++ src/contracts/core/AllocationManagerStorage.sol
@@ -108,5 +108,5 @@ abstract contract AllocationManagerStorage is IAllocationManager {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[38] private __gap;
+    uint256[36] private __gap;
 }
